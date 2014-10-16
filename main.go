e43e7efc14ed693052176fdef74701307751a71c
Rag Sagar.V <ragsagar@gmail.com>
2014-10-16 10:59:10 +0400

    Redesigned bot with a struct type Bot

--- main.go
+++ main.go
@@ -3,25 +3,68 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"net/textproto"
 	"os"
 	"strings"
 )
 
 type Bot struct {
-	conn            *textproto.Conn
-	server, channel string
-	port            int16
+	conn                                  *textproto.Conn
+	server, port, channel, nick, username string
+	done                                  chan bool
+	reader                                *bufio.Reader
 }
 
-func NewBot(server, port, channel, nick, username) {
+func NewBot(server, port, channel, nick, username string) *Bot {
+	return &Bot{
+		server:   server,
+		port:     port,
+		channel:  channel,
+		nick:     nick,
+		username: username,
+	}
 }
 
-func ListenToMessage(conn *textproto.Conn, done chan bool) {
+func (bot Bot) Connect() {
+	fmt.Printf("Connecting to %s .. ", bot.server)
+	connection_string := net.JoinHostPort(bot.server, bot.port)
+	conn, err := textproto.Dial("tcp", connection_string)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bot.conn = conn
+	go bot.ListenToMessage()
+	fmt.Println(bot)
+	bot.conn.PrintfLine("NICK %s", bot.nick)
+	bot.conn.PrintfLine("USER %s 0 * :%s", bot.nick, bot.username)
+	bot.conn.PrintfLine("JOIN %s", bot.channel)
+	bot.SendMessage("Hi")
+	bot.SendUserInput()
+	<-bot.done
+	fmt.Println("Quitting")
+}
+
+func (bot Bot) SendMessage(message string) {
+	bot.conn.PrintfLine("PRIVMSG %s :%s", bot.channel, message)
+}
+
+func (bot Bot) SendCommand(message string) {
+	bot.Quit()
+}
+
+func (bot Bot) Quit() {
+	bot.conn.PrintfLine("QUIT :bye")
+	bot.done <- true
+	bot.conn.Close()
+}
+
+func (bot Bot) ListenToMessage() {
 	for {
-		message, err := conn.ReadLine()
+		message, err := bot.conn.ReadLine()
 		if err != nil {
-			done <- true
+			bot.conn.Close()
 		}
 		fmt.Println(message)
 		index := strings.Index(message, "PING")
@@ -29,35 +72,28 @@ func ListenToMessage(conn *textproto.Conn, done chan bool) {
 			parts := strings.Split(message, ":")
 			response := "PONG " + parts[1]
 			fmt.Println("Received PING, responding with ", response)
-			conn.PrintfLine(response)
+			bot.conn.PrintfLine(response)
 		}
 	}
 }
 
-func SendUserInput(conn *textproto.Conn) {
-	reader := bufio.NewReader(os.Stdin)
+func (bot Bot) SendUserInput() {
+	bot.reader = bufio.NewReader(os.Stdin)
 	for {
 
-		input, _ := reader.ReadString('\n')
-		conn.PrintfLine("PRIVMSG #botwar :" + input)
+		user_input, _ := bot.reader.ReadString('\n')
+		// if user input starts with / that means it is a command.
+		index := strings.Index(user_input, "/")
+		if index == 0 {
+			bot.SendCommand(user_input)
+		}
+		bot.SendMessage(user_input)
 	}
 }
 
 func main() {
-	done := make(chan bool)
-	fmt.Println("Connecting to freenode")
-	new_conn, err := textproto.Dial("tcp", "irc.freenode.net:6667")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer new_conn.Close()
-	go ListenToMessage(new_conn, done)
 
-	new_conn.PrintfLine("NICK ragsagar1")
-	new_conn.PrintfLine("USER ragsagar1 0 * :Bot")
+	bot := NewBot("irc.freenode.net", "6667", "#botwar", "ragsag1", "Botty")
+	bot.Connect()
 
-	new_conn.PrintfLine("JOIN #botwar")
-	new_conn.PrintfLine("PRIVMSG #botwar :Hi")
-	go SendUserInput(new_conn)
-	<-done
 }
